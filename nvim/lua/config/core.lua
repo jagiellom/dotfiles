8a7b1654cc6ee09af02946619c94478b61e13fbf
Michal Jagiello <jagiello.m@gmail.com>
2025-06-01 13:43:42 +0200

    config update

--- nvim/lua/config/core.lua
+++ nvim/lua/config/core.lua
@@ -4,12 +4,13 @@ vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.laststatus = 3
- 
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.ruler = false
+vim.g.omni_sql_no_default_maps = 1
 
- 
+
 vim.cmd "set noshowmode"
 
 vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
@@ -23,3 +24,5 @@ vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
     border = 'rounded',
   }
 )
+
+vim.diagnostic.config({ virtual_lines = true })
--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -4,7 +4,7 @@ return {
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     { "antosha417/nvim-lsp-file-operations", config = true },
-    { "folke/neodev.nvim", opts = {} },
+    { "folke/neodev.nvim",                   opts = {} },
   },
   config = function()
     local mason = require("mason")
@@ -52,5 +52,8 @@ return {
       capabilities = capabilities
     })
 
+    lspconfig.pylsp.setup({
+      capabilities = capabilities
+    })
   end,
 }
